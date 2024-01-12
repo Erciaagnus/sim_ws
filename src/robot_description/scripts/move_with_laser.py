193b2c351cf47e4f5b9b02ac8b3057b23e9d2c42
Jeong Hyeok Lim <henricus0973@gmail.com>
2024-01-12 15:54:04 +0900

    Added formatter

--- src/robot_description/scripts/move_with_laser.py
+++ src/robot_description/scripts/move_with_laser.py
@@ -2,26 +2,28 @@
 
 import rospy
 from geometry_msgs.msg import Twist
-from  sensor_msgs.msg import LaserScan
+from sensor_msgs.msg import LaserScan
+
 
 def laser_callback(msg):
-    msg=LaserScan()
-    rospy.loginfo('Minimum distance is:%s' % min(msg.ranges) )
+    rospy.loginfo("Minimum distance is:%s" % min(msg.ranges))
+
 
 def main():
-    laser_sub=rospy.Subscriber('laser/scan', LaserScan, callback=laser_callback)
-    twist_pub=rospy.Publisher('cmd_vel', Twist, queue_size=10)
-    rospy.init_node('move_with_laser_node', anonymous= True)
-    rate=rospy.Rate(10) #10Hz
+    laser_sub = rospy.Subscriber("laser/scan", LaserScan, callback=laser_callback)
+    twist_pub = rospy.Publisher("cmd_vel", Twist, queue_size=10)
+    rospy.init_node("move_with_laser_node", anonymous=True)
+    rate = rospy.Rate(10)  # 10Hz
 
     while not rospy.is_shutdown():
-        twist_msg=Twist()
-        twist_msg.linear.x=0.3
-        twist_msg.angular.z=0.3
+        twist_msg = Twist()
+        twist_msg.linear.x = 0.3
+        twist_msg.angular.z = 0.3
         twist_pub.publish(twist_msg)
         rate.sleep()
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     try:
         main()
     except rospy.ROSInterruptException:
